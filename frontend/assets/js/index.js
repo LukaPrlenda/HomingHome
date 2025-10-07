e68ae448edde26a5939e8d95e478c09d43b11b13
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-10-07 23:52:47 +0200

    Added menu highlighting

--- frontend/assets/js/index.js
+++ frontend/assets/js/index.js
@@ -7,32 +7,32 @@ app.run();
 
 app.route({
     view: "page_Main",
-    onReady: function() {runCustom();}
+    onReady: function() {runCustom(); navigationMenu("#page_Main");}
 });
 
 app.route({
     view: "page_Contact",
-    onReady: function() {runCustom();}
+    onReady: function() {runCustom(); navigationMenu("#page_Contact");}
 });
 
 app.route({
     view: "page_Properties",
-    onReady: function() {runCustom();}
+    onReady: function() {runCustom(); navigationMenu("#page_Properties");}
 });
 
 app.route({
     view: "page_Property-details",
-    onReady: function() {runCustom();}
+    onReady: function() {runCustom(); navigationMenu("#page_Property-details");}
 });
 
 app.route({
     view: "page_Signup",
-    onReady: function() {runCustom(); start_signup_js();}
+    onReady: function() {runCustom(); navigationMenu("#page_Signup"); start_signup_js();}
 });
 
 app.route({
     view: "page_Login",
-    onReady: function() {runCustom();}
+    onReady: function() {runCustom(); navigationMenu("#page_Login");}
 });
 /*
 
