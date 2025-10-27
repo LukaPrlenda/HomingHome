c5070054c8302c84d76544f7c9f3e81b29215cf4
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-10-27 23:08:29 +0100

    Add method and fix mistakes in listingDao

--- backend/rest/dao/listingDao.php
+++ backend/rest/dao/listingDao.php
@@ -15,23 +15,27 @@ class ListingDao extends BaseDao{
     }
 
     public function get_address_by_status($status){
-        return $this->query('SELECT p.location, l.id AS listing_id, l.property_id FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status])
+        return $this->query('SELECT p.location, l.id AS listing_id, l.property_id FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status]);
     }
 
     public function get_by_status($status){
-        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status])
+        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status]);
     }
 
     public function get_first_by_status($status){
-        return $this->query_unique('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status])
+        return $this->query_unique('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status]);
     }
 
-    public function get_first_by_type($type){
-        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE t.type = :type', ['type' => $type])
+    public function get_first_by_type_and_status($type, $status){
+        return $this->query_unique('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE l.status = :status AND t.type = :type', ['status' => $status, 'type' => $type]);
     }
 
     public function get_by_type_and_status($type, $status){
-        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE l.status = :Active AND t.type = :type', ['status' => $status, 'type' => $type])
+        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE l.status = :status AND t.type = :type', ['status' => $status, 'type' => $type]);
+    }
+
+    public function get_first_N_of_status($status, $number){
+        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE l.status = :status LIMIT ' . $number, ['status' => $status]);
     }
 
 
