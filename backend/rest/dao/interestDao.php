43079f027c9d6311976be25edae935b91d7dc7f4
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-10-27 19:18:36 +0100

    Add interestDao

---
+++ backend/rest/dao/interestDao.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/baseDao.php';
+
+class InterestDao extends BaseDao{
+    protected $table_name;
+
+    public function __construct(){
+        $this->table_name = "interests";
+        parent::__construct($this->table_name);
+    }
+
+    public function get_all(){
+        return $this->query('SELECT * FROM ' . $this->table_name, []);
+    }
+
+    public function get_by_status($status){
+        return $this->query('SELECT p.*, i.id AS interest_id, i.user_id AS interested_user_id, i.status, i.message FROM interests i JOIN properties p ON i.property_id = p.id WHERE i.status = :status', ['status'=> $status]);
+    }
+
+    public function get_by_status_and_intrested_id($status, $user_id){
+        return $this->query('SELECT p.location FROM ' . $this->table_name . ' i JOIN properties p ON i.property_id = p.id WHERE i.user_id = :user_id AND i.status = :status', ['status' => $status, 'user_id' => $user_id]);
+    }
+
+    public function get_by_status_and_owner_id($status, $user_id){
+        return $this->query('SELECT p.*, i.id AS interest_id, i.user_id AS intrested_user_id, i.status, i.message FROM ' . $this->table_name . ' i JOIN properties p ON i.property_id = p.id WHERE p.user_id = :user_id AND i.status = :status', ['status' => $status, 'user_id' => $user_id]);
+    }
+}
+?>
--- backend/rest/dao/listingDao.php
+++ backend/rest/dao/listingDao.php
@@ -22,6 +22,14 @@ class ListingDao extends BaseDao{
         return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status])
     }
 
+    public function get_first_by_status($status){
+        return $this->query_unique('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id WHERE l.status = :status', ['status' => $status])
+    }
+
+    public function get_first_by_type($type){
+        return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE t.type = :type', ['type' => $type])
+    }
+
     public function get_by_type_and_status($type, $status){
         return $this->query('SELECT p.*, l.status, l.id AS listing_id, t.type FROM ' . $this->table_name . ' l JOIN properties p ON l.property_id = p.id JOIN types t ON t.id = p.type_id  WHERE l.status = :Active AND t.type = :type', ['status' => $status, 'type' => $type])
     }
