cf49a16dc64a4859a239531da94fbc1b06903979
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-10-27 23:39:38 +0100

    Add admin_messagesDao

---
+++ backend/rest/dao/admin_messagesDao.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/baseDao.php';
+
+class Admin_messagesDao extends BaseDao{
+    protected $table_name;
+
+    public function __construct(){
+        $this->table_name = "admin_messages";
+        parent::__construct($this->table_name);
+    }
+
+    public function get_all(){
+        return $this->query('SELECT *  FROM ' . $this->table_name, []);
+    }
+
+    public function get_by_user_id($user_id){
+        return $this->query('SELECT *  FROM ' . $this->table_name . 'WHERE user_id = :user_id', ['user_id' => $user_id]);
+    }
+
+    public function get_by_property_id($property_id){
+        return $this->query('SELECT *  FROM ' . $this->table_name . 'WHERE property_id = :property_id', ['property_id' => $property_id]);
+    }
+}
+?>
