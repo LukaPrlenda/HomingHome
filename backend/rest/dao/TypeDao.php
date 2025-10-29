ee06595e019fa9fff5205fac673f7115bfe46bdc
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-10-29 22:28:05 +0100

    Add TypeDao

---
+++ backend/rest/dao/TypeDao.php
@@ -0,0 +1,19 @@
+<?php
+include_once __DIR__ . '/BaseDao.php';
+
+class UserDao extends BaseDao{
+    protected $table_name;
+
+    public function __construct(){
+        $this->table_name = "types";
+        parent::__construct($this->table_name);
+
+    }
+
+ 
+
+    public function get_by_type($type){
+        return $this->query('SELECT * FROM ' . $this->table_name . ' WHERE type = :type', ['type' => $type]);
+    }
+}
+?>
