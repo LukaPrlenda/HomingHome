787aa694b6302ab6617a257f0fc6fb6d58326a0f
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-11-07 20:05:15 +0100

    Add initial Swagger

---
+++ backend/public/v1/docs/doc_setup.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+*  @OA\Info(
+*      title="API",
+*      description="HomingHome API",
+*      version="1.0",
+ 
+*      @OA\Contact(
+*          email="luka.prlenda@stu.ibu.edu.ba",
+*          name="LP"
+*      )
+*  )
+*/
+/**
+*   @OA\Server(
+*       url="LOCALSERVER",
+*       description="API server"
+*   )
+*/
+/**
+*   @OA\SecurityScheme(
+*       securityScheme="ApiKey",
+*       type="apiKey",
+*       in="header",
+*       name="Authentication"
+*       )
+*/
+?>
