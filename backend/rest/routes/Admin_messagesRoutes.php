bcc6055176747e250ae785d9370dd2095f3235a2
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-11-08 21:42:25 +0100

    Add Swagger annotations in Admin_messagesRoutes

--- backend/rest/routes/Admin_messagesRoutes.php
+++ backend/rest/routes/Admin_messagesRoutes.php
@@ -1,17 +1,99 @@
 <?php
 
+/**
+ *  @OA\Get(
+ *      path="/admin_messages/user/{id}",
+ *      tags={"admin_messages"},
+ *      summary="Fetch the admin messages by user ID.",
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required=true,
+ *          description="User ID",
+ *          @OA\Schema(type="integer", example=1)
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Fetch the admin messages for user."
+ *      )
+ *  )
+ */
 Flight::route('GET /admin_messages/user/@id', function($id){
     Flight::json(Flight::admin_messagesService()->get_by_user_id($id));
 });
 
+/**
+ *  @OA\Get(
+ *      path="/admin_messages/property/{id}",
+ *      tags={"admin_messages"},
+ *      summary="Fetch the admin messages by property ID.",
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required=true,
+ *          description="Property ID",
+ *          @OA\Schema(type="integer", example=1)
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Fetch the admin messages for property."
+ *      )
+ *  )
+ */
 Flight::route('GET /admin_messages/property/@id', function($id){
     Flight::json(Flight::admin_messagesService()->get_by_property_id($id));
 });
 
+/**
+ *  @OA\Get(
+ *      path="/admin_messages",
+ *      tags={"admin_messages"},
+ *      summary="Fetch all admin messages",
+ *      @OA\Response(
+ *          response=200,
+ *          description="Fetch the admin messages."
+ *      )
+ *  )
+ */
 Flight::route('GET /admin_messages', function(){
     Flight::json(Flight::admin_messagesService()->get_all_admin_messages());
 });
 
+/**
+ *  @OA\Post(
+ *      path="/admin_messages",
+ *      tags={"admin_messages"},
+ *      summary="Add a new admin message",
+ *      @OA\RequestBody(
+ *          required=true,
+ *          @OA\JsonContent(
+ *              required={"user_id", "property_id", "message"},
+ *              @OA\Property(
+ *                  property="user_id",
+ *                  type="integer",
+ *                  example=1,
+ *                  description="Message for user with this ID."
+ *              ),
+ *              @OA\Property(
+ *                  property="property_id",
+ *                  type="integer",
+ *                  example=1,
+ *                  description="Message regarding property with this ID."
+ *              ),
+ *              @OA\Property(
+ *                  property="message",
+ *                  type="string",
+ *                  example="This is a message from admin",
+ *                  description="Message content"
+ *              )
+ *          )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Admin message added successfully."
+ *      )
+ *  )
+ */
 Flight::route('POST /admin_messages', function(){
     $request = Flight::request()->data->getData();
 
@@ -21,7 +103,55 @@ Flight::route('POST /admin_messages', function(){
     ]);
 });
 
-Flight::route('PUT /admin_messages/@id', function($id){
+/**
+ *  @OA\Patch(
+ *      path="/admin_messages/{id}",
+ *      tags={"admin_messages"},
+ *      summary="Update an existing admin message",
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required=true,
+ *          description="Admin message ID",
+ *          @OA\Schema(type="integer", example=1)
+ *      ),
+ *      @OA\Parameter(
+ *          name="id_column",
+ *          in="query",
+ *          required=false,
+ *          description="Column to use as ID",
+ *          @OA\Schema(type="string", example="id")
+ *      ),
+ *      @OA\RequestBody(
+ *          required=true,
+ *          @OA\JsonContent(
+ *              @OA\Property(
+ *                  property="user_id",
+ *                  type="integer",
+ *                  example=1,
+ *                  description="Message for user with this ID."
+ *              ),
+ *              @OA\Property(
+ *                  property="property_id",
+ *                  type="integer",
+ *                  example=1,
+ *                  description="Message regarding property with this ID."
+ *              ),
+ *              @OA\Property(
+ *                  property="message",
+ *                  type="string",
+ *                  example="This is a message from admin",
+ *                  description="Message content"
+ *              )
+ *          )
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Admin message edited successfully."
+ *      )
+ *  )
+ */
+Flight::route('PATCH /admin_messages/@id', function($id){
     $data = Flight::request()->data->getData();
     $id_column = Flight::request()->query['id_column'] ?? "id";
     
@@ -31,6 +161,24 @@ Flight::route('PUT /admin_messages/@id', function($id){
     ]);
 });
 
+/**
+ *  @OA\Delete(
+ *      path="/admin_messages/{id}",
+ *      tags={"admin_messages"},
+ *      summary="Delete the admin message by ID",
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          required=true,
+ *          description="Admin message ID",
+ *          @OA\Schema(type="integer", example=1)
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="Admin message deleted successfully."
+ *      )
+ *  )
+ */
 Flight::route('DELETE /admin_messages/@id', function($id){
     Flight::admin_messagesService()->delete_admin($id);
     Flight::json(['message' => "Admin message deleted successfully"]);
