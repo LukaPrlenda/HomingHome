c1dabad565eb52f653e93232f230965cf4b19e59
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-11-26 00:58:56 +0100

    Add AuthRoutes

--- backend/index.php
+++ backend/index.php
@@ -3,6 +3,7 @@
 require 'vendor/autoload.php';
 
 require_once __DIR__ . '/rest/services/Admin_messagesService.php';
+require_once __DIR__ . '/rest/services/AuthService.php';
 require_once __DIR__ . '/rest/services/InterestService.php';
 require_once __DIR__ . '/rest/services/ListingService.php';
 require_once __DIR__ . '/rest/services/PropertiesService.php';
@@ -11,6 +12,7 @@ require_once __DIR__ . '/rest/services/UserService.php';
 
 
 Flight::register('admin_messagesService', 'Admin_messagesService');
+Flight::register('authService', 'AuthService');
 Flight::register('interestService', 'InterestService');
 Flight::register('listingService', 'ListingService');
 Flight::register('propertiesService', 'PropertiesService');
@@ -19,6 +21,7 @@ Flight::register('userService', 'UserService');
 
 
 require_once __DIR__ . '/rest/routes/Admin_messagesRoutes.php';
+require_once __DIR__ . '/rest/routes/AuthRoutes.php';
 require_once __DIR__ . '/rest/routes/InterestRoutes.php';
 require_once __DIR__ . '/rest/routes/ListingRoutes.php';
 require_once __DIR__ . '/rest/routes/PropertiesRoutes.php';
