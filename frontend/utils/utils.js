158b896a8c0805bc3bbafd95bbf49f98da9179b6
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-12-02 00:48:06 +0100

    Add utils.js

---
+++ frontend/utils/utils.js
@@ -0,0 +1,15 @@
+let Utils = {
+    parseJwt: function(token) {
+        if(!token)
+            return null;
+        try {
+            const payload = token.split('.')[1];
+            const decoded = atob(payload);
+            return JSON.parse(decoded);
+        }
+        catch (e) {
+            console.log("Invalid JWT token", e);
+            return null;
+        }
+    }
+}
