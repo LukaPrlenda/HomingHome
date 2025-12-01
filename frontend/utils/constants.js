50d60563cd8169b26dec1c27af2c2790d99ca023
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-12-01 19:53:42 +0100

    Add constants.js to frontend

---
+++ frontend/utils/constants.js
@@ -0,0 +1,5 @@
+const Constants = {
+    PROJECT_BASE_URL: location.hostname == "localhost" ? "http://http://localhost/HomingHome/backend/" : "https://add-production/",
+    USER_ROLE: "user",
+    ADMIN_ROLE: "admin"
+}
