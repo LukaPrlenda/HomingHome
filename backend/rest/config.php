a81764d07c556bdbb73a52dfa46c143827a544b5
Luka Prlenda <luka.prlenda@stu.ibu.edu.ba>
2025-12-27 15:02:35 +0100

    Change URL in utils.js

--- backend/rest/config.php
+++ backend/rest/config.php
@@ -26,7 +26,7 @@ class Config{
     }
 
     public static function JWT_SECRET(){
-        return Config::get_env("JWT_SECRET", "test");
+        return Config::get_env("JWT_SECRET", "");
     }
 
     public static function get_env($name, $default){
